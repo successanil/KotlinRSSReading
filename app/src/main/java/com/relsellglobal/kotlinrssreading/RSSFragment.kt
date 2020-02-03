476cd9fa335d71db941683b785217b21d85f5c9d
Relsell <relsellglobal@gmail.com>
2020-02-03 13:26:24 +0530

    code cleanup

--- app/src/main/java/com/relsellglobal/kotlinrssreading/RSSFragment.kt
+++ app/src/main/java/com/relsellglobal/kotlinrssreading/RSSFragment.kt
@@ -44,16 +44,6 @@ class RSSFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_item_list, container, false)
 
-//        // Set the adapter
-//        if (view is RecyclerView) {
-//            with(view) {
-//                layoutManager = when {
-//                    columnCount <= 1 -> LinearLayoutManager(context)
-//                    else -> GridLayoutManager(context, columnCount)
-//                }
-//
-//            }
-//        }
         listV = view.findViewById(R.id.listV)
         return view
     }
@@ -119,17 +109,7 @@ class RSSFragment : Fragment() {
     }
 
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson
-     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
+
     interface OnListFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onListFragmentInteraction(item: RssItem?)
